d838d68f6708c7373186dc98e59770c756f744b2
bkerrpeterson <bkerrpeterson@gmail.com>
2017-03-24 16:54:24 +0000

    Made a change
    Made a change in the code

--- DestroyTheWorld/ViewController.swift
+++ DestroyTheWorld/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Say Hello World")
+        print("Made this change")
     }
 
     override func didReceiveMemoryWarning() {
